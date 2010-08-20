bfd46b01305e42bba085e24fac2501ab9dfd1e21
Vernkin Chen <vernkin@gmail.com>
2010-08-20 10:43:55 +0800

    add some tet

---
+++ source/boosttest/bt_icma_core.cpp
@@ -0,0 +1,17 @@
+/**
+ * \file bt_icma_core.cpp
+ * \brief 
+ * \date Aug 20, 2010
+ * \author Vernkin Chen
+ */
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(icma_core_test)
+
+BOOST_AUTO_TEST_CASE(icma_core)
+{
+    BOOST_CHECK( ((1 + 1) < 2) );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
